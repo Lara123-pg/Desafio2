43aee2349e30fd25569b1be5d59e13254fcc859a
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-07-20 02:02:47 -0300

    feat:add a attribute

--- src/app/app/(main)/types.ts
+++ src/app/app/(main)/types.ts
@@ -1,8 +1,9 @@
 export type Todo = {
-  id: string
-  title: string
-  userId: string
-  createdAt: Date
-  updatedAt: Date
-  doneAt: Date | null
+  id: string;
+  title: string;
+  description?: string;
+  userId: string;
+  createdAt: Date;
+  updatedAt: Date;
+  doneAt: Date | null;
 }
