6a99f396444aa74427f339f1da683a714379d82f
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-07-21 10:58:59 -0300

    feat:structure of the note modal

---
+++ src/components/modal/ModalPage.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react'
+import { cn } from '@/lib/utils'
+
+export type ModalPageGenericProps<T = unknown> = {
+  children: ReactNode;
+  className?: string;
+} & T
+
+export function ModalPage({ className, children }: ModalPageGenericProps) {
+    return (
+      <div className={cn('fixed inset-0 bg-black/60 flex items-center justify-center', className)}>
+        {children}
+      </div>
+    )
+}
+
+export function ModalContainer({ className, children }: ModalPageGenericProps) {
+  return (
+    <div className={cn('w-[640px] rounded-xl py-5 px-6 bg-white space-y-5', className)}>
+      {children}
+    </div>
+  )
+}
+
+export function ModalHeader({ className, children }: ModalPageGenericProps) {
+  return <div className={cn('flex items-center justify-between space-y-1', className)}>{children}</div>
+}
+
+export function ModalTitle({ className, children }: ModalPageGenericProps) {
+  return <h1 className={cn('text-2xl font-bold', className)}>{children}</h1>
+}
+
+export function ModalDescription({ className, children }: ModalPageGenericProps) {
+  return (
+    <p className={cn('text-gray-500 dark:text-gray-400', className)}>
+      {children}
+    </p>
+  )
+}
