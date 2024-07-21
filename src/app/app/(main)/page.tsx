786d19c7d8d9ba8ca710674230b8ace7831af214
lara123-pg <lamorimalvescavalcante@gmail.com>
2024-07-21 10:59:33 -0300

    feat: modifications of hook

--- src/app/app/(main)/page.tsx
+++ src/app/app/(main)/page.tsx
@@ -15,14 +15,34 @@ import {
 import { FileText } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { PlusIcon } from '@radix-ui/react-icons'
-import useFetchNotes from '@/hooks/useFetchNotes'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ModalNote from './components/ModalNote'
+import { getNotes } from '@/requests/note/getNotes'
+import { Note } from '@/schemas/note'
+import Link from 'next/link'
 
 export default function Page() {
   const [isOpenModal, setIsOpenModal] = useState(false);
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [isUpdated, setIsUpdated] = useState(false)
 
-  const { notes } = useFetchNotes();
+  useEffect(() => {
+    const fetchNotes = async () => {
+        try {
+            const response = await getNotes();
+
+            if (response.data) {
+              setNotes(response.data)
+            }
+
+        } catch(error) {
+            console.error("Erro ao buscar notas")
+        }
+    }
+
+    fetchNotes();
+  }, [isUpdated]) 
+
 
   return (
     <DashboardPage>
@@ -32,29 +52,34 @@ export default function Page() {
           <DashboardPageHeaderNav>
             <Button variant="outline" size="sm" onClick={() => setIsOpenModal(true)}>
               <PlusIcon className="mr-3 h-4 w-4" />
-              Criar notas
+              Criar nota
             </Button>
           </DashboardPageHeaderNav>
         </DashboardPageHeaderNav>
       </DashboardPageHeader>
       <DashboardPageMain>
         {
-          notes.length == 0 ? (
+          notes.length === 0 ? (
             <div className="flex flex-col items-center gap-1 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <FileText size={60} />
               <p>Nenhuma nota encontrada</p>
             </div>
           ) : (
             notes.map(note => (
-              <Card key={note.id}>
+              <Link key={note.id} href={`/noteDetails/${note.id}`}>
+                <Card className="cursor-pointer hover:bg-slate-50">
                   <CardTitle>{note.title}</CardTitle>
-              </Card>
+                </Card>
+              </Link>
             ))
           )}
 
           {
             isOpenModal && (
-              <ModalNote />
+              <ModalNote 
+                setIsOpenModal={setIsOpenModal}
+                setIsUpdated={setIsUpdated}
+              />
             )
           }
       </DashboardPageMain>
